b923a1b30e73ce52727f298a859aa8e4bfb94729
LMFM <61855440+margaritasing@users.noreply.github.com>
2022-03-07 17:32:53 -0300

    cambio la key de la api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '75b9f04bb9ba776a3e2318bbe7838f21',
+    apiKey: 'aquivalaapidelsitio',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
